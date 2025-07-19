4f1c46e1e0503dcdf90618fbdb8afd1ac93c6cb8
Drew Lee <drew@spoonlabs.com>
2025-07-19 17:33:32 +0900

    feat: book entity

--- src/features/steps/ui/StepRating.tsx
+++ src/features/steps/ui/StepRating.tsx
@@ -2,7 +2,7 @@ export const StepRating = () => {
   return (
     <section>
       <h2>2단계 - 도서 추천 여부, 별점</h2>
-      <p>폼 입력 필드는 이후 PR에서 추가됩니다.</p>
+      {/* 폼 입력 필드는 이후 PR에서 추가됩니다. */}
     </section>
   );
 };
--- src/widgets/book-form/consts/form-default-values.ts
+++ src/widgets/book-form/consts/form-default-values.ts
@@ -1,10 +1,12 @@
-export const formDefaultValues = {
+import { BookFormData, BookStatus } from "@/entities/book";
+
+export const formDefaultValues: BookFormData = {
     title: '',
-    status: '',
+    status: BookStatus.WANT_TO_READ,
     startDate: '',
     endDate: '',
     rating: 0,
     review: '',
     quotes: [],
     isPublic: false,
-  }
+};
